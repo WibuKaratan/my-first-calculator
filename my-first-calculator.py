50c7c3dbd1a4cdeb8a9159ac94c1ec9efa3a6db7
WibuKaratan <wibukaratans@gmail.com>
2025-11-29 19:34:03 +0700

    Kalkukator doang loh

---
+++ my-first-calculator.py
@@ -0,0 +1,10 @@
+#just a normal calculator but without trigonometry and square root
+
+print("==Kalkulator==")
+print("==Silahkan Gunakan dan/atau tanda tanda operasi matematika berikut ini==")
+print("== Tambah ( + ) | Kurang ( - ) | Kali ( * ) | Bagi ( / ) == ")
+print("==Format: 1+1 | 1/1 ==") #Format
+
+while True : #Pengulangan
+ Hitung = eval(input(" ")) #penghitungan 
+ print("= " + str( Hitung)) #hasil
